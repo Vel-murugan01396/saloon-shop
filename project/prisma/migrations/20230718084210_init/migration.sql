42603df2244cfbe3674baf234b9ad330018e4c86
Vel-murugan01396 <126084681+Vel-murugan01396@users.noreply.github.com>
2024-01-30 14:06:13 +0530

    Add files via upload

---
+++ project/prisma/migrations/20230718084210_init/migration.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE "login" (
+    "id" TEXT NOT NULL,
+  --   "firstname "  TEXT,
+  -- "secondname"      TEXT,
+  -- "creatpassword"   TEXT,
+    "password" TEXT,
+    "user"     TEXT,
+
+    CONSTRAINT "login_pkey" PRIMARY KEY ("id")
+);
